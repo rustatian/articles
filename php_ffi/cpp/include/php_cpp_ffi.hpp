4bee71c7df28542fd8a5c1c0dee9c2e94d2a55b3
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-04-01 23:58:06 +0300

    Article update, code update

---
+++ php_ffi/cpp/include/php_cpp_ffi.hpp
@@ -0,0 +1,8 @@
+//
+// Created by valery on 4/1/20.
+//
+
+#ifndef CPP_PHP_CPP_FFI_HPP
+#define CPP_PHP_CPP_FFI_HPP
+
+#endif //CPP_PHP_CPP_FFI_HPP
--- php_ffi/golang/go_php_ffi.go
+++ php_ffi/golang/go_php_ffi.go
@@ -1,2 +1,19 @@
+package main
+
+import (
+	"C"
+)
+
+// we need to have empty main in package main :)
 func main() {
+
+}
+
+//export Fib
+func Fib(n C.int) C.int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+
+	return Fib(n-1) + Fib(n-2)
 }
