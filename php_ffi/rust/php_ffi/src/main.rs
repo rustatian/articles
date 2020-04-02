b7cb6899516c06afa75df41b8a6ed6dc56c77f78
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-04-03 00:48:24 +0300

    update

--- php_ffi/rust/php_ffi/src/main.rs
+++ php_ffi/rust/php_ffi/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+
+#[no_mangle]
+extern "C" fn Fib(n: i32) -> i32 {
+    if (n == 0) || (n == 1) {
+        return 1;
+    }
+
+    Fib(n - 1) + Fib(n - 2)
 }
